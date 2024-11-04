5759c5ca8c389d8996aae956ec92ebbfd077f22b
Abdusamad Abdusattorov <abdusattorov.coder@gmail.com>
2024-11-04 12:26:43 +0100

    Initial Commit

---
+++ WingSpan/WingSpanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WingSpanApp.swift
+//  WingSpan
+//
+//  Created by Abdusamad Abdusattorov on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WingSpanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
